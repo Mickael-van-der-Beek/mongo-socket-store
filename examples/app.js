6857d16236ae2fa0771007ef9209d9347ad9333d
mickael van der beek <kollektiv@kollektiv.local>
2013-11-10 01:47:28 +0100

    cleaning-up the repo and added basic features and an example

--- examples/app.js
+++ examples/app.js
@@ -1,15 +1,46 @@
+// The following code is only necessary to run the example index.html
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app);
+
+app.configure(function () {
+  // Use static file if no controllers were able to respond to the request
+  app.use(express.static(__dirname + '/client'));
+});
+
+app.post('/', function (req, res) {
+  fs.createReadStream('client/index.html').pipe(res);
+});
+
+// Start of the relevant parts of the code
 var MongoStore = require('../index')
   , Mongo = require('mongodb')
-  , io = require('socket.io').listen(8000, {
-  	log: true
+  , io = require('socket.io').listen(server, {
+    log: true
   });
 
 io.configure(function () {
-	io.set('store', new MongoStore({
-		nodeId: null,
-		mongo: null,
-		host: 'localhost',
-		port: 27017,
-		mongoClient: null
-	}));
+  io.set('store', new MongoStore({
+    nodeId: null,
+    mongo: null,
+    host: 'localhost',
+    port: 27017,
+    mongoClient: null
+  }));
 });
+
+io.sockets.on('connection', function (socket) {
+  console.log('\nConnection to Socket.io initialized !');
+
+  socket.emit('request', { message: 'Hello World!' });
+
+  socket.on('response', function (data) {
+    console.log('\nResponse from client received !');
+    console.log(data);
+  });
+});
+
+// Make Express.js and Socket.io listen on port 8000
+server.listen(8000);
+console.log('\nExpress.js and Socket.io listening on port 8000 !');
